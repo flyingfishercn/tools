a3a1ecd42c1db99e056f909470f36d21f866301e
zhiquan.huang <flyingfishercn@gmail.com>
2013-12-19 19:37:21 +0800

    add my favoriate little software

--- monkeymemorytest/testmonkey/temp/monkeyRecorder - 副本.py
+++
@@ -1,4 +0,0 @@
-from com.android.monkeyrunner import MonkeyRunner
-from com.android.monkeyrunner.recorder import MonkeyRecorder
-device = MonkeyRunner.waitForConnection()
-MonkeyRecorder.start(device)
